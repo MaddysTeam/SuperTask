f368497a887d2b5e720acb3bcc123908dc0ae140
zhenghc@sou.edu.cn <zhenghch@sou.edu.cn>
2019-01-03 17:34:03 +0800

    change ..wait a monment

--- SuperTask.Site/Business/DataDef/Payments.cs
+++ SuperTask.Site/Business/DataDef/Payments.cs
@@ -7,7 +7,7 @@ namespace Business
    public partial class Payments
    {
 
-      public bool IsProjectType => this.PayType == PaymentsKeys.ProjectPaymentsType;
+      public bool IsProjectType => this.PayType == PaymentsKeys.ProjectPaymentsType; 
       public bool IsInternalVenderType => this.PayType == PaymentsKeys.InternalVenderPaymentsType;
    }
 
