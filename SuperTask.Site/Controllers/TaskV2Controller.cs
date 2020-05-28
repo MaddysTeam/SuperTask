dce6a4d931b8c9eb7cb85c1d43710fe830194fda
JimmyPoor <kissnana3@163.com>
2020-05-28 18:08:59 +0800

    20200528

---
+++ SuperTask.Site/Controllers/TaskV2Controller.cs
@@ -0,0 +1,70 @@
+﻿using Business;
+using Business.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TheSite.Controllers
+{
+
+   public class TaskV2Controller : BaseController
+   {
+		
+		public ActionResult List()
+		{
+			return View();
+		}
+
+
+		[HttpPost]
+		public ActionResult List(int current, int rowCount, AjaxOrder sort, string searchPhrase)
+		{
+			return Json(new { });
+		}
+
+
+		public ActionResult Edit()
+		{
+			return View();
+		}
+
+
+		[HttpPost]
+		public ActionResult Delete(Guid id)
+		{
+			return Json(new { });
+		}
+
+
+		[HttpPost]
+		public ActionResult EditChild()
+		{
+			return Json(new { });
+		}
+
+
+		[HttpPost]
+		public ActionResult Start()
+		{
+			return Json(new { });
+		}
+
+		[HttpPost]
+		public ActionResult Complete()
+		{
+			return Json(new { });
+		}
+
+
+		[HttpPost]
+		public ActionResult Close()
+		{
+			return Json(new { });
+		}
+		
+	
+   }
+
+}
