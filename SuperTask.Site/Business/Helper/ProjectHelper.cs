3a847e489fd89a2b1fe428a5fe71c42fe87af4af
JimmyPoor <kissnana3@163.com>
2020-06-12 16:00:02 +0800

    20200612

--- SuperTask.Site/Business/Helper/ProjectHelper.cs
+++ SuperTask.Site/Business/Helper/ProjectHelper.cs
@@ -7,103 +7,104 @@ using System.Linq;
 namespace Business.Helper
 {
 
-   public static class ProjectrHelper
-   {
-      /// <summary>
-      /// 某用户参与的所有项目
-      /// </summary>
-      /// <param name="userId">用户id</param>
-      /// <param name="db"></param>
-      /// <returns></returns>
-      public static List<Project> UserJoinedProjects(Guid userId, APDBDef db)
-      {
-         var p = APDBDef.Project;
-         var re = APDBDef.Resource;
+	public static class ProjectrHelper
+	{
+		/// <summary>
+		/// 某用户参与的所有项目
+		/// </summary>
+		/// <param name="userId">用户id</param>
+		/// <param name="db"></param>
+		/// <returns></returns>
+		public static List<Project> UserJoinedProjects(Guid userId, APDBDef db)
+		{
+			var p = APDBDef.Project;
+			var re = APDBDef.Resource;
 
-         var projects = APQuery.select(p.ProjectId, p.ProjectName)
-                .from(p,
-                      re.JoinInner(re.Projectid == p.ProjectId & re.UserId == userId))
-					  .order_by(p.CreateDate.Desc)
-                .query(db, r => new Project
-                {
-                   ProjectId = p.ProjectId.GetValue(r),
-                   ProjectName = p.ProjectName.GetValue(r)
-                }).ToList();
+			var projects = APQuery.select(p.ProjectId, p.ProjectName,p.CreateDate)
+				   .from(p,
+						 re.JoinInner(re.Projectid == p.ProjectId & re.UserId == userId))
+						 .order_by(p.CreateDate.Desc)
+						 .distinct()
+				   .query(db, r => new Project
+				   {
+					   ProjectId = p.ProjectId.GetValue(r),
+					   ProjectName = p.ProjectName.GetValue(r)
+				   }).ToList();
 
-         return projects;
-      }
+			return projects;
+		}
 
-      /// <summary>
-      /// 通过项目id获取当前的项目数据
-      /// </summary>
-      /// <param name="projectid">项目id</param>
-      /// <param name="db"></param>
-      /// <param name="isforceClear"></param>
-      /// <returns></returns>
-      public static Project GetCurrentProject(Guid projectid, APDBDef db = null, bool isforceClear = false)
-      {
-         db = db ?? new APDBDef();
-         return db.ProjectDal.PrimaryGet(projectid);
-      }
-
-
-      /// <summary>
-      /// 通过项目节点任务的完成率来得到项目进度
-      /// </summary>
-      /// <param name="projectId">项目id</param>
-      /// <param name="db"></param>
-      /// <returns></returns>
-      public static  double GetProcessByNodeTasks(Guid projectId,APDBDef db)
-      {
-         var pst = APDBDef.ProjectStoneTask;
-         var all = db.ProjectStoneTaskDal.ConditionQueryCount(pst.ProjectId== projectId);
-         var completed = db.ProjectStoneTaskDal.ConditionQueryCount(pst.ProjectId == projectId & pst.TaskStatus == TaskKeys.CompleteStatus);
-
-         return all <= 0 ? 0 : ((double)(completed * 100 / all)).Round(2);
-      }
+		/// <summary>
+		/// 通过项目id获取当前的项目数据
+		/// </summary>
+		/// <param name="projectid">项目id</param>
+		/// <param name="db"></param>
+		/// <param name="isforceClear"></param>
+		/// <returns></returns>
+		public static Project GetCurrentProject(Guid projectid, APDBDef db = null, bool isforceClear = false)
+		{
+			db = db ?? new APDBDef();
+			return db.ProjectDal.PrimaryGet(projectid);
+		}
 
 
-   }
+		/// <summary>
+		/// 通过项目节点任务的完成率来得到项目进度
+		/// </summary>
+		/// <param name="projectId">项目id</param>
+		/// <param name="db"></param>
+		/// <returns></returns>
+		public static double GetProcessByNodeTasks(Guid projectId, APDBDef db)
+		{
+			var pst = APDBDef.ProjectStoneTask;
+			var all = db.ProjectStoneTaskDal.ConditionQueryCount(pst.ProjectId == projectId);
+			var completed = db.ProjectStoneTaskDal.ConditionQueryCount(pst.ProjectId == projectId & pst.TaskStatus == TaskKeys.CompleteStatus);
+
+			return all <= 0 ? 0 : ((double)(completed * 100 / all)).Round(2);
+		}
 
 
-   //public class ProjectRecordHelper
-   //{
+	}
 
-   //   static APDBDef.ProjectRecordTableDef prt = APDBDef.ProjectRecord;
 
-   //   public static void CreateRecord(Project proj, Guid operatorId, APDBDef db)
-   //   {
-   //      db.ProjectRecordDal.Insert(new ProjectRecord
-   //      {
-   //         RecordId = Guid.NewGuid(),
-   //         RecordDate = DateTime.Now,
-   //         StartDate = proj.StartDate,
-   //         EndDate = proj.EndDate,
-   //         Progress = proj.RateOfProgress,
-   //         ProjectStatus = proj.ProjectStatus,
-   //         ProjectType = proj.ProjectType,
-   //         ProjectId = proj.ProjectId,
-   //         RateOfProgress = proj.RateOfProgress,
-   //         PMId = proj.PMId,
-   //         ManagerId = proj.ManagerId,
-   //         ReviewerId = proj.ReviewerId,
-   //         OperatorId = operatorId,
-   //         ProjectExecutor = proj.ProjectExecutor,
-   //         ProjectOwner = proj.ProjectOwner,
-   //         RealCode = proj.RealCode,
-   //         Code = proj.Code,
-   //         ProjectName = proj.ProjectName,
-   //         ProcessName = proj.ProcessName
-   //      });
-   //   }
+	//public class ProjectRecordHelper
+	//{
 
-   //   public static void CreateRecord(Project proj, Project orignal, Guid operatorId, APDBDef db)
-   //   {
-   //      if (!proj.Equals(orignal))
-   //         CreateRecord(proj, operatorId, db);
-   //   }
+	//   static APDBDef.ProjectRecordTableDef prt = APDBDef.ProjectRecord;
 
-   //}
+	//   public static void CreateRecord(Project proj, Guid operatorId, APDBDef db)
+	//   {
+	//      db.ProjectRecordDal.Insert(new ProjectRecord
+	//      {
+	//         RecordId = Guid.NewGuid(),
+	//         RecordDate = DateTime.Now,
+	//         StartDate = proj.StartDate,
+	//         EndDate = proj.EndDate,
+	//         Progress = proj.RateOfProgress,
+	//         ProjectStatus = proj.ProjectStatus,
+	//         ProjectType = proj.ProjectType,
+	//         ProjectId = proj.ProjectId,
+	//         RateOfProgress = proj.RateOfProgress,
+	//         PMId = proj.PMId,
+	//         ManagerId = proj.ManagerId,
+	//         ReviewerId = proj.ReviewerId,
+	//         OperatorId = operatorId,
+	//         ProjectExecutor = proj.ProjectExecutor,
+	//         ProjectOwner = proj.ProjectOwner,
+	//         RealCode = proj.RealCode,
+	//         Code = proj.Code,
+	//         ProjectName = proj.ProjectName,
+	//         ProcessName = proj.ProcessName
+	//      });
+	//   }
+
+	//   public static void CreateRecord(Project proj, Project orignal, Guid operatorId, APDBDef db)
+	//   {
+	//      if (!proj.Equals(orignal))
+	//         CreateRecord(proj, operatorId, db);
+	//   }
+
+	//}
 
 
 }
--- SuperTask.Site/Business/Helper/WorkJournalHelper.cs
+++ SuperTask.Site/Business/Helper/WorkJournalHelper.cs
@@ -6,90 +6,98 @@ using System.Web;
 namespace Business.Helper
 {
 
-   public static class WorkJournalHelper
-   {
-      static APDBDef.WorkJournalTableDef wj = APDBDef.WorkJournal;
+	public static class WorkJournalHelper
+	{
+		static APDBDef.WorkJournalTableDef wj = APDBDef.WorkJournal;
 
-      /// <summary>
-      /// 通过任务创建或修改日志
-      /// </summary>
-      /// <param name="tk">任务实体</param>
-      /// <param name="db">APDBDef</param>
-      public static void CreateOrUpdateJournalByTask(WorkTask tk, APDBDef db)
-      {
-         if (tk == null || db == null || !tk.IsProcessStatus) return;
+		/// <summary>
+		/// 通过任务创建或修改日志
+		/// </summary>
+		/// <param name="tk">任务实体</param>
+		/// <param name="db">APDBDef</param>
+		public static void CreateOrUpdateJournalByTask(WorkTask tk, APDBDef db)
+		{
+			if (tk == null || db == null || !tk.IsProcessStatus) return;
 
-         var jounal = db.WorkJournalDal
-            .ConditionQuery(wj.TaskId == tk.TaskId
-                          & wj.RecordDate > DateTime.Now.TodayStart()
-                          & wj.RecordDate < DateTime.Now.TodayEnd(), null, null, null)
-            .FirstOrDefault();
+			var jounal = db.WorkJournalDal
+			   .ConditionQuery(wj.TaskId == tk.TaskId
+							 & wj.UserId == tk.ManagerId
+							 & wj.RecordDate > DateTime.Now.TodayStart()
+							 & wj.RecordDate < DateTime.Now.TodayEnd(), null, null, null)
+			   .FirstOrDefault();
 
-         if (jounal == null)
-            CreateByTask(tk, db);
-         else
-            db.WorkJournalDal.UpdatePartial(jounal.JournalId,
-               new
-               {
-                  UserId = tk.ManagerId,
-                  Projectid = tk.Projectid,
-                  TaskId = tk.TaskId,
-                  Status = JournalKeys.SaveStatus,
-                 // RecordType = GetRecordType(tk),
-                  TaskStatus = tk.TaskStatus,
-                  //TaskType = tk.TaskType,
-                  //TaskSubType = tk.SubType,
-                  //Progress = tk.RateOfProgress,
-               });
-      }
-
-
-      /// <summary>
-      /// 通过任务集合创建日志
-      /// </summary>
-      /// <param name="tasks">任务集合</param>
-      /// <param name="db">APDBDef</param>
-      public static void CreateOrUpdateJournalByTasks(List<WorkTask> tasks, APDBDef db)
-      {
-         if (tasks == null || tasks.Count <= 0 || db == null)
-            return;
-
-         foreach (var tk in tasks)
-         {
-            CreateOrUpdateJournalByTask(tk, db);
-         }
-      }
+			if (jounal == null)
+				CreateByTask(tk, db);
+			else
+				db.WorkJournalDal.UpdatePartial(jounal.JournalId,
+				   new
+				   {
+					   UserId = tk.ManagerId,
+					   Projectid = tk.Projectid,
+					   TaskId = tk.TaskId,
+					   Status = JournalKeys.SaveStatus,
+					   RecordType = JournalKeys.ManuRecordType,
+					   TaskStatus = tk.TaskStatus,
+					   TaskType = tk.TaskType,
+					   //TaskSubType = tk.SubType,
+					   //Progress = tk.RateOfProgress,
+				   });
+		}
 
 
-      public static WorkJournal CreateByTask(WorkTask tk, APDBDef db)
-      {
-         var journal = new WorkJournal
-         {
-            JournalId = Guid.NewGuid(),
-            UserId = tk.ManagerId,
-            Projectid = tk.Projectid,
-            TaskId = tk.TaskId,
-            TaskEstimateWorkHours = tk.EstimateWorkHours,
-            WorkHours = 0,
-            Progress = tk.RateOfProgress,//default value is 0
-            Comment = string.Empty,
-            CreateDate = DateTime.Now,
-            RecordDate = DateTime.Now,
-            Status = JournalKeys.SaveStatus,
-            RecordType = GetRecordType(tk),
-            TaskStatus = tk.TaskStatus,
-            TaskType = tk.TaskType,
-            TaskSubType=tk.SubTypeId
-         };
+		/// <summary>
+		/// 通过任务集合创建日志
+		/// </summary>
+		/// <param name="tasks">任务集合</param>
+		/// <param name="db">APDBDef</param>
+		public static void CreateOrUpdateJournalByTasks(List<WorkTask> tasks, APDBDef db)
+		{
+			if (tasks == null || tasks.Count <= 0 || db == null)
+				return;
 
-         db.WorkJournalDal.Insert(journal);
+			foreach (var tk in tasks)
+			{
+				CreateOrUpdateJournalByTask(tk, db);
+			}
+		}
 
-         return journal;
-      }
 
-      public static Guid GetRecordType(WorkTask tk)
-      => tk.IsParent ? JournalKeys.AutoRecordType : JournalKeys.ManuRecordType;
+		public static WorkJournal CreateByTask(WorkTask tk, APDBDef db)
+		{
+			var journal = new WorkJournal
+			{
+				JournalId = Guid.NewGuid(),
+				UserId = tk.ManagerId,
+				Projectid = tk.Projectid,
+				TaskId = tk.TaskId,
+				TaskEstimateWorkHours = tk.EstimateWorkHours,
+				WorkHours = 0,
+				Progress = tk.RateOfProgress,//default value is 0
+				Comment = string.Empty,
+				CreateDate = DateTime.Now,
+				RecordDate = DateTime.Now,
+				Status = JournalKeys.SaveStatus,
+				RecordType = GetRecordType(tk),
+				TaskStatus = tk.TaskStatus,
+				TaskType = tk.TaskType,
+				TaskSubType = tk.SubTypeId
+			};
 
-   }
+			db.WorkJournalDal.Insert(journal);
+
+			return journal;
+		}
+
+		public static Guid GetRecordType(WorkTask tk)
+		=> tk.IsParent ? JournalKeys.AutoRecordType : JournalKeys.ManuRecordType;
+
+		public static WorkJournal GetJournal(Guid taskId, Guid userId, APDBDef db)
+			=> db
+		   .WorkJournalDal
+		   .ConditionQuery(wj.TaskId == taskId & wj.UserId == userId & wj.RecordDate.Between(DateTime.Now.TodayStart(), DateTime.Now.TodayEnd()), null, null, null)
+		   .FirstOrDefault();
+
+
+	}
 
 }
